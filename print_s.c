24111f12001087030d7305bb7a486f6224e28aa4
jonseb974 <5151@holbertonschool.com>
2022-07-08 22:26:28 +0200

    print_s_test1

---
+++ print_s.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stddef.h>
+/**
+ * print_s - print string
+ * @la_list: Store the list of characters
+ * Return: num of parameters printed
+ */
+
+int print_s(va_list la_list)
+{
+	int i, count = 0;
+	char *s;
+
+	s = va_arg(la_list, char *);
+	if (s == NULL)
+		return (-1);
+	for (i = 0; s[i]; i++)
+		count++;
+	_putchar(s[i]);
+
+	return (count);
+}
