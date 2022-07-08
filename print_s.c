635ede600917bb8ef3474c1ea2657538ceefd22b
jonseb974 <5151@holbertonschool.com>
2022-07-08 22:37:56 +0200

    print_s_test7

--- print_s.c
+++ print_s.c
@@ -3,7 +3,7 @@
 /**
  * print_s - print string
  * @la_list: Store the list of characters
- * Return: number of parameters printed
+ * Return: numbers of parameters printed
  */
 
 int print_s(va_list la_list)
